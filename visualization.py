1ccca7dcae970cb03391e04059b58ef7a642be0e
dstushar7 <35169303+dstushar7@users.noreply.github.com>
2021-06-17 20:02:01 +0600

    datasets and predictions and visualization
    slowly we are going for the prediction

---
+++ visualization.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from datetime import datetime as dt
+import itertools
+from pandas.plotting import scatter_matrix
+
+dataset = pd.read_csv('dataset.csv')
+
+
+#correlation matrix for dataset
+# plt.figure(figsize=(20,10)) 
+# sns.heatmap(dataset.corr(), annot=True)
+# plt.show(block=True)
+
+
+# Remove few column
+dataset2 = dataset.copy().drop(columns =['Date', 'HomeTeam', 'AwayTeam', 'FTHG', 'FTAG',
+       'HTGS', 'ATGS', 'HTGC', 'ATGC',
+       'HM4', 'HM5','AM4', 'AM5', 'MW', 'HTFormPtsStr',
+       'ATFormPtsStr', 'HTFormPts', 'ATFormPts', 'HTWinStreak3',
+       'HTWinStreak5', 'HTLossStreak3', 'HTLossStreak5', 'ATWinStreak3',
+       'ATWinStreak5', 'ATLossStreak3', 'ATLossStreak5',
+       'DiffPts'] )
+
+
+# Total number of matches.
+n_matches = dataset2.shape[0]
+
+# Calculate number of features. -1 because we are saving one as the target variable (win/lose/draw)
+n_features = dataset2.shape[1] - 1
+
+# Calculate matches won by home team.
+n_homewins = len(dataset2[dataset2.FTR == 'H'])
+
+# Calculate win rate for home team.
+win_rate = (float(n_homewins) / (n_matches)) * 100
+
+# Print the results
+print("Total number of matches: {}".format(n_matches))
+print ("Number of features: {}".format(n_features))
+print( "Number of matches won by home team: {}".format(n_homewins))
+print ("Win rate of home team: {:.2f}%".format(win_rate))
+
+
+
+# Plotting the scatter matrix
+# scatter_matrix(dataset2[['HTGD','ATGD','HTP','ATP','DiffFormPts']], figsize=(15,15))
+# plt.show(block=True)
